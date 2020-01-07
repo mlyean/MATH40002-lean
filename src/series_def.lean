12afd8186bbf84ae4ac46107f706db2af577c879
Lim Ming Yean <mingyean@hotmail.com>
2020-01-07 22:04:38 +0000

    chap4: divergence of series def

--- src/series_def.lean
+++ src/series_def.lean
@@ -13,6 +13,10 @@ def sum_to_inf_eq (a : seq) := is_limit (partial_sum a)
 
 def sum_to_inf_converges (a : seq) := seq_converges (partial_sum a)
 
+def sum_to_inf_diverges (a : seq) := seq_diverges (partial_sum a)
+def sum_to_inf_diverges_to_pos_inf (a : seq) := seq_diverges_to_pos_inf (partial_sum a)
+def sum_to_inf_diverges_to_neg_inf (a : seq) := seq_diverges_to_neg_inf (partial_sum a)
+
 end sec_4_1
 
 end real_series
