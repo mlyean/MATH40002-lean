14b26455a4037b41e01db3a965c6cc372ea946b9
Lim Ming Yean <mingyean@hotmail.com>
2020-01-07 05:59:08 +0000

    chap4: theorem 4.2

--- src/series.lean
+++ src/series.lean
@@ -10,11 +10,10 @@ open real_series
 section sec_4_1
 
 -- Example 4.1
-lemma sum_of_geom_series (x : ℝ) (hx : abs x < 1) : is_limit (geom_series x ∘ (+ 1)) (1 / (1 - x)) := begin
+lemma sum_of_geom_series (x : ℝ) (hx : abs x < 1) : is_limit (geom_series x) (1 / (1 - x)) := begin
   have hx' : x ≠ 1 := ne_of_lt (abs_lt.mp hx).2,
-  have h : geom_series x ∘ (+ 1) = λ n, (1 - x ^ (n + 1)) / (1 - x) := begin
+  have h : geom_series x = λ n, (1 - x ^ n) / (1 - x) := begin
     funext n,
-    change geom_series x (n + 1) = _,
     rw [geom_sum hx', ←neg_div_neg_eq, neg_sub, neg_sub],
   end,
   rw h,
@@ -26,8 +25,24 @@ lemma sum_of_geom_series (x : ℝ) (hx : abs x < 1) : is_limit (geom_series x �
   },
   replace hx' := sub_ne_zero.mpr hx'.symm,
   refine lim_div_eq_div_lim hx' (lim_sub_eq_sub_lim lim_of_const_seq _) lim_of_const_seq,
-  rw ←lim_eq_lim_of_tail 1,
   exact lim_of_geom_zero' (abs_lt.mp hx),
+end
+
+lemma partial_sum_succ_sub (a : seq) (n : ℕ) : partial_sum a (n + 1) - partial_sum a n = a n := begin
+  unfold partial_sum,
+  rw finset.sum_Ico_succ_top (nat.zero_le n),
+  exact add_sub_cancel' (partial_sum a n) (a n),
+end
+
+-- Theorem 4.2 (Limit test)
+theorem lim_of_terms_eq_zero (a : seq) (ha : sum_to_inf_converges a) : is_limit a 0 := begin
+  have h : a = λ n, partial_sum a (n + 1) - partial_sum a n := begin
+    funext n,
+    exact (partial_sum_succ_sub a n).symm,
+  end,
+  cases ha with l hl,
+  rw [h, ←sub_self l],
+  exact lim_sub_eq_sub_lim ((lim_eq_lim_of_tail 1).mp hl) hl,
 end
 
 end sec_4_1
