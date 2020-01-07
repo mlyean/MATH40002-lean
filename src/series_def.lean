14c53bd8e5f0c898566ac1c88d20f988b8cf3096
Lim Ming Yean <mingyean@hotmail.com>
2020-01-07 05:57:54 +0000

    chap4: add defs

--- src/series_def.lean
+++ src/series_def.lean
@@ -7,7 +7,11 @@ open real_seq
 
 section sec_4_1
 
-def partial_sum (a : seq) : seq := λ n, (finset.Ico 0 (n + 1)).sum a
+def partial_sum (a : seq) : seq := λ n, (finset.Ico 0 n).sum a
+
+def sum_to_inf_eq (a : seq) := is_limit (partial_sum a)
+
+def sum_to_inf_converges (a : seq) := seq_converges (partial_sum a)
 
 end sec_4_1
 
