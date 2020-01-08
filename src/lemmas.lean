d2ffc20f8677bf7e23578972f1de51dbe0aee16e
Lim Ming Yean <mingyean@hotmail.com>
2020-01-08 10:02:39 +0000

    move to lemmas

--- src/lemmas.lean
+++ src/lemmas.lean
@@ -2,3 +2,32 @@ import data.real.basic
 
 lemma div_lt_iff'' {a b c : ℝ} (hb : b > 0) (hc : c > 0) : a / b < c ↔ a / c < b :=
   by rw [div_lt_iff hb, div_lt_iff' hc]
+
+lemma Sup_subset_le_Sup {A B : set ℝ} (h : B ⊆ A) (ha_bdd : bdd_above A) (hb_nonempty : B ≠ ∅) : real.Sup B ≤ real.Sup A := begin
+  refine real.Sup_le_ub B (set.exists_mem_of_ne_empty hb_nonempty) _,
+  intros x hx,
+  exact real.le_Sup A ha_bdd (h hx),
+end
+
+lemma Inf_le_Inf_subset {A B : set ℝ} (h : B ⊆ A) (ha_bdd : bdd_below A) (hb_nonempty : B ≠ ∅) : real.Inf A ≤ real.Inf B := begin
+  refine real.lb_le_Inf B (set.exists_mem_of_ne_empty hb_nonempty) _,
+  intros x hx,
+  exact real.Inf_le A ha_bdd (h hx),
+end
+
+lemma range_add_eq_Ici (n : ℕ) : set.range (+ n) = set.Ici n := begin
+  refine set.ext _,
+  intro x,
+  simp,
+  split,
+  { rintro ⟨y, hy⟩,
+    exact nat.le.intro hy,
+  },
+  { exact nat.le.dest }
+end
+
+lemma range_add_subset_range_add {m n : ℕ} (h : m ≤ n) : set.range (+ n) ⊆ set.range (+ m) := begin
+  rw [range_add_eq_Ici, range_add_eq_Ici],
+  intros x hx,
+  exact le_trans h hx,
+end
--- src/seq.lean
+++ src/seq.lean
@@ -738,40 +738,11 @@ lemma bdd_below_of_cauchy {a : seq} : seq_cauchy a → seq_bdd_below a :=
   λ ha, seq_bdd_below_of_bdd $ bdd_of_cauchy ha
 
 -- Some lemmas for Theorem 3.19
-lemma Sup_subset_le_Sup {A B : set ℝ} (h : B ⊆ A) (ha_bdd : bdd_above A) (hb_nonempty : B ≠ ∅) : real.Sup B ≤ real.Sup A := begin
-  refine real.Sup_le_ub B (set.exists_mem_of_ne_empty hb_nonempty) _,
-  intros x hx,
-  exact real.le_Sup A ha_bdd (h hx),
-end
-
-lemma Inf_le_Inf_subset {A B : set ℝ} (h : B ⊆ A) (ha_bdd : bdd_below A) (hb_nonempty : B ≠ ∅) : real.Inf A ≤ real.Inf B := begin
-  refine real.lb_le_Inf B (set.exists_mem_of_ne_empty hb_nonempty) _,
-  intros x hx,
-  exact real.Inf_le A ha_bdd (h hx),
-end
-
 lemma bdd_above_of_tail {a : seq} (k : ℕ) (ha : seq_bdd_above a) : seq_bdd_above (a ∘ (+ k)) := 
   bdd_above_subset (set.range_comp_subset_range (+ k) a) ha
 
 lemma bdd_below_of_tail {a : seq} (k : ℕ) (ha : seq_bdd_below a) : seq_bdd_below (a ∘ (+ k)) := 
   bdd_below_subset (set.range_comp_subset_range (+ k) a) ha
-
-lemma range_add_eq_Ici (n : ℕ) : set.range (+ n) = set.Ici n := begin
-  refine set.ext _,
-  intro x,
-  simp,
-  split,
-  { rintro ⟨y, hy⟩,
-    exact nat.le.intro hy,
-  },
-  { exact nat.le.dest }
-end
-
-lemma range_add_subset_range_add {m n : ℕ} (h : m ≤ n) : set.range (+ n) ⊆ set.range (+ m) := begin
-  rw [range_add_eq_Ici, range_add_eq_Ici],
-  intros x hx,
-  exact le_trans h hx,
-end
 
 lemma Inf_decreasing_eq_Inf_tail {a : seq} (n : ℕ) (ha_decr : seq_decreasing a) (ha_bdd : seq_bdd_below a) :
   real.Inf (set.range a) = real.Inf (set.range (a ∘ (+ n))) :=
