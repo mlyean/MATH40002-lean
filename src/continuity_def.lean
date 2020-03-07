881b2a681907b4deb83d85c963405c5c9ee0edac
Lim Ming Yean <mingyean@hotmail.com>
2020-03-07 12:35:31 +0000

    add continuous

--- src/continuity_def.lean
+++ src/continuity_def.lean
@@ -12,6 +12,8 @@ section sec_5_2
 
 def continuous_at (f : ℝ → ℝ) (a : ℝ) := ∀ ε > 0, ∃ δ > 0, ∀ x, abs (x - a) < δ → abs (f x - f a) < ε
 
+def continuous (f : ℝ → ℝ) := ∀ a, continuous_at f a
+
 end sec_5_2
 
 end continuity
